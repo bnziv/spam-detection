4b86249810063ae32fd90ef26e608eccb8e60fca
Benzi <benzi.villaruel@gmail.com>
2025-01-31 03:02:13 -0500

    Add env support and fix styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
+const apiUrl = import.meta.env.VITE_API_URL
+
 function App() {
   const [text, setText] = useState('');
   const [result, setResult] = useState('');
   const [probability, setProbability] = useState({});
   const [error, setError] = useState('');
-  
+
   const handleSubmit = async () => {
     if (!text.trim()) return;
 
     setError('');
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/predict', {
+      const response = await fetch(apiUrl, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text }),
@@ -36,12 +38,12 @@ function App() {
     <div className="flex items-center justify-center min-h-screen p-4 flex-col">
       <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6">
         <h1 className={`text-2xl font-bold text-center
-        ${result === 'spam' ? 'text-yellow-500' : result === 'ham' ? 'text-green-500' : result === 'phishing' ? 'text-red-500' : 'text-gray-500'}`}>
+        ${result === 'spam' ? 'text-yellow-500' : result === 'ham' ? 'text-green-500' : result === 'phishing' ? 'text-red-500' : 'text-black'}`}>
           Spam Detection
         </h1>
 
         <textarea
-          className="w-full h-32 p-3 mt-4 border border-gray-300 rounded-lg focus:ring focus:ring-blue-300 resize-none"
+          className="w-full h-32 p-3 mt-4 border border-gray-300 rounded-lg focus:ring focus:ring-blue-300 resize-none text-black"
           placeholder="Enter text here..."
           value={text}
           onChange={(e) => setText(e.target.value)}
@@ -60,25 +62,25 @@ function App() {
           Submit
         </button>
       </div>
-      <div className="w-full max-w-md bg-white shadow-md rounded-lg p-6 mt-4">
       {result && (
           <div className={`mt-4 p-3 text-center font-semibold text-white rounded-lg
           ${result === 'spam' ? 'bg-yellow-500' : result === 'ham' ? 'bg-green-500' : result === 'phishing' ? 'bg-red-500' : 'bg-gray-500'}`}>
-            {result.toUpperCase()} {(probability[result] * 100).toPrecision(2)}%
+            {result.toUpperCase()} {parseInt(probability[result] * 100)}%
             
             {/* Display other probabilities if the main one is less than 90% */}
             {probability[result] < 0.9 &&
-            Object.entries(probability).map(([label, prob]) => (
+            Object.entries(probability)
+            .sort((a, b) => b[1] - a[1])
+            .map(([label, prob]) => (
               label !== result &&
               <div key={label}>
-                {label.toUpperCase()}: {(prob * 100).toPrecision(2)}%
+                {label.toUpperCase()}: {parseInt(prob * 100)}%
               </div>
             ))
             }
           </div>
         )}
       </div>
-    </div>
   )
 }
 
